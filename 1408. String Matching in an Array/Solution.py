77edf07f955b2374a9f29bff92623739e0e13996
Hiren Bhanushali <hirenbhanushalihb1@gmail.com>
2020-04-12 09:45:15 +0530

    Create Solution.py

---
+++ 1408. String Matching in an Array/Solution.py
@@ -0,0 +1,48 @@
+'''
+Given an array of string words. Return all strings in words which is substring of another word in any order. 
+
+String words[i] is substring of words[j], if can be obtained removing some characters to left and/or right side of words[j].
+
+ 
+
+Example 1:
+
+Input: words = ["mass","as","hero","superhero"]
+Output: ["as","hero"]
+Explanation: "as" is substring of "mass" and "hero" is substring of "superhero".
+["hero","as"] is also a valid answer.
+
+Example 2:
+
+Input: words = ["leetcode","et","code"]
+Output: ["et","code"]
+Explanation: "et", "code" are substring of "leetcode".
+
+Example 3:
+
+Input: words = ["blue","green","bu"]
+Output: []
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 30
+words[i] contains only lowercase English letters.
+It's guaranteed that words[i] will be unique.
+'''
+
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        l = words.copy()
+        b = []
+        for i in range(len(words)):
+            l.pop(i)
+            for j in l:
+                a = words[i].find(j)
+                if a!=-1:
+                    z = b.count(j)
+                    if (z==0):
+                        b.append(j)
+            l = words.copy()
+        return b
